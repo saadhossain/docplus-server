ec93f897375d909df3a6d1b98bd132f7ff776181
saadhossain <mshsarkar@gmail.com>
2022-11-23 16:10:54 +0600

    Accesstoken, api for save and get users

--- index.js
+++ index.js
@@ -13,10 +13,35 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@firstmongodb.yjij5fj.mongodb.net/?retryWrites=true&w=majority`
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+//JWT Token for verification
+const jwt = require('jsonwebtoken')
+app.post('/accesstoken', async(req, res)=> {
+    const user = req.body;
+    const token = jwt.sign(user, process.env.ACCESS_TOKEN)
+    res.send({accessToken: token})
+})
+//Verify JWT token
+const verifiyJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send({message: 'unauthorized access'})
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+        if(err){
+            return res.status(403).send({message: 'unauthorized access'})
+        }
+        req.decoded = decoded;
+        next()
+    })
+}
+
 //Database connection
 const dbConnect = () => {
     const treatmentsData = client.db('docPlus').collection('treatments')
     const appointmentBooking = client.db('docPlus').collection('appointments');
+    //User collection
+    const usersData = client.db('docPlus').collection('users');
     //Post appointments data to the mongodb
     app.post ('/appointments', async(req, res)=> {
         const newAppoint = req.body;
@@ -33,6 +58,7 @@ const dbConnect = () => {
         const result = await appointmentBooking.insertOne(newAppoint)
         res.send(result)
     })
+    
 
     //Get treatement options from the database
     app.get('/treatmentoptions', async(req, res)=> {
@@ -50,6 +76,36 @@ const dbConnect = () => {
             treatment.slots = remainingSchedule;
         })
         res.send(treatments)
+    })
+
+    //Get appointments for a specific user by email
+    app.get('/appointments',verifiyJWT, async(req, res)=> {
+        const email = req.query.email;
+        const query = {email: email}
+        const decodedEmail = req.decoded.email;
+        if(email !== decodedEmail){
+            return res.status(403).send({message: 'forbidden access'})
+        }
+        const appointments = await appointmentBooking.find(query).toArray()
+        res.send(appointments)
+    })
+
+    //Save users to the database
+    app.post('/users', async(req, res)=> {
+        const user = req.body;
+        const result = await usersData.insertOne(user)
+        res.send(result)
+    })
+    //Get users from database
+    app.get('/users', verifiyJWT, async(req, res)=> {
+        const query = {}
+        const email = req.query.email;
+        const decodedEmail = req.decoded.email;
+        if(email !== decodedEmail){
+            return res.status(403).send({message: 'forbidden access'})
+        }
+        const users = await usersData.find(query).toArray()
+        res.send(users)
     })
 }
 
